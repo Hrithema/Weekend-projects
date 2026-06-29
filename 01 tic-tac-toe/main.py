59d12bffd2ea1415a88066a803282961477f98d2
Hrithema <143249287+Hrithema@users.noreply.github.com>
2026-06-29 22:28:41 +0530

    Add draw detection and prevent invalid moves

--- 01 tic-tac-toe/main.py
+++ 01 tic-tac-toe/main.py
@@ -1,4 +1,4 @@
-def sum(a, b, c):
+def line_sum(a, b, c):
     return a + b + c
 
 def print_board(xState, zState):
@@ -11,14 +11,17 @@ def print_board(xState, zState):
 def checkWin(xState, zState):
     wins = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
     for win in wins:
-        if (sum(xState[win[0]], xState[win[1]], xState[win[2]])==3):
+        if (line_sum(xState[win[0]], xState[win[1]], xState[win[2]])==3):
             print("X won the match!")
             return 1
         
-        if (sum(zState[win[0]], zState[win[1]], zState[win[2]])==3):
+        if (line_sum(zState[win[0]], zState[win[1]], zState[win[2]])==3):
             print("O won the match!")
             return 0
     return -1
+
+def check_draw(xState, zState):
+    return sum(xState) + sum(zState) == 9
         
 if __name__ == "__main__":
     xState = [0,0,0,0,0,0,0,0,0]  
@@ -32,12 +35,28 @@ if __name__ == "__main__":
         if turn == 1:
             print("X's turn.")
             value = int(input("Enter a value: "))
-            xState[value] = 1
+
+            if xState[value] == 0 and zState[value] == 0:
+                xState[value] = 1
+            else:
+                print("Cell already occupied. Try again.")
+                continue
+
         else:
             print("O's turn.")
             value = int(input("Enter a value: "))
-            zState[value] = 1
+
+            if xState[value] == 0 and zState[value] == 0:
+                zState[value] = 1
+            else:
+                print("Cell already occupied. Try again.")
+                continue
         cwin = checkWin(xState, zState)
         if (cwin != -1):
+            break
+        
+        draw = check_draw(xState, zState)
+        if draw:
+            print("It's a Draw")
             break
         turn = 1 - turn  # Switch turns
